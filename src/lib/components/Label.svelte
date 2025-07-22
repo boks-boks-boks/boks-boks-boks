4670600d25a28d05412ebc52c531a4f7fe69c0a7
vincentastolfi <vincent.astolfi@etu.uca.fr>
2025-07-22 20:34:01 +0200

    feat: create label component

---
+++ src/lib/components/Label.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    let {children, color = '#FFFFFF'} = $props()
+
+    let textColor: string = $state(backgroundColorToTextColor(color))
+
+    function backgroundColorToTextColor(colorCode: string): string {
+        let colorWithoutHash = colorCode.slice(1);
+        let r = Number("0x"+colorWithoutHash.substring(0, 2))
+        let g = Number("0x"+colorWithoutHash.substring(2, 4))
+        let b = Number("0x"+colorWithoutHash.substring(4, 6))
+
+        return r*0.299 + g*0.587 + b*0.114 > 120 ? "#000000" : "#ffffff"
+    }
+</script>
+
+<p class="label" style="background-color: {color}; color: {textColor}">
+    {@render children?.()}
+</p>
+
+<style>
+    .label {
+        display: inline-block;
+		padding: 0.3rem 0.8rem;
+		border-radius: 20px;
+		text-decoration: none;
+		font-weight: 450;
+        cursor: default;
+		text-align: center;
+		border: none;
+		font-size: 0.8rem;
+		font-family: inherit;
+		line-height: 1;
+    }
+</style>
---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import Label from '$lib/components/Label.svelte'
+</script>
+
+<Label color="#d1d5db">Test</Label>
+<Label color="#FF0000">zgdqvdqvdhqvdqv</Label>
+<Label color="#FFFFFF">Test</Label>
+<Label color="#00FF00">Test</Label>
+<Label color="#FFFF00">Test</Label>
+<Label color="#000000">Test</Label>
+<Label color="#0ce123">Test</Label>
+<Label color="#88cfd5">Test</Label>
+<Label color="#144246">Test</Label>
