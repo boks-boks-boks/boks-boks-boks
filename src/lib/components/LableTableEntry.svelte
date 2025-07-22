9d4a171d2370ab603849cf81bce4a56a151fa86e
vincentastolfi <vincent.astolfi@etu.uca.fr>
2025-07-22 21:37:51 +0200

    feat: base label page ui

---
+++ src/lib/components/LableTableEntry.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+    let {children} = $props()
+</script>
+
+<div class="entry-border">
+    <div class="entry-container">
+        <div class="left-container">
+            {@render children?.()}
+
+            <p>descriptive</p>
+        </div>
+        <div class="right-container">
+            <span>🏷️</span>
+            <span>✏️</span>
+        </div>
+    </div>
+</div>
+
+<style>
+    .entry-border {
+        border-color: #e3e4e5;
+        border-style: solid;
+        border-width: 1px;
+    }
+
+    .entry-container {
+        background-color: white;
+        border-radius: 10px;
+        display: flex;
+        justify-content: space-between;
+        flex-direction: row;
+        padding: 0 1rem 0 1rem
+    }
+
+    .left-container {
+        display: flex;
+        align-items: center;
+        gap: 9rem;
+    }
+
+    .right-container {
+        display: flex;
+        align-items: center;
+        gap: 1.5rem
+    }
+</style>
---
+++ src/routes/labels/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import Label from "$lib/components/Label.svelte";
+    import LableTableEntry from "$lib/components/LableTableEntry.svelte";
+
+    let mockedLables = [
+        {"title": "outil", "color": "#259361"},
+        {"title": "outil", "color": "#259361"},
+        {"title": "outil", "color": "#259361"},
+        {"title": "outil", "color": "#259361"},
+    ]
+</script>
+
+{#each mockedLables as label}
+    <LableTableEntry><Label color={label.color}>{label.title}</Label></LableTableEntry>
+{/each}
