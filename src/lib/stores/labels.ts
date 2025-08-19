bf534c18ca4f17c413cc5a29fe91a995007b07b5
vincentastolfi <vincent.astolfi@etu.uca.fr>
2025-08-19 19:36:45 +0200

    fix: remove useless console.log

--- src/lib/stores/labels.ts
+++ src/lib/stores/labels.ts
@@ -3,8 +3,6 @@ import { type LabelModel } from "$lib";
 
 export const userLabels = writable<LabelModel[] | null>(null);
 
-userLabels.subscribe(s => console.log(JSON.parse(JSON.stringify(s))))
-
 export function setLabels(labels: LabelModel[]): void {
     userLabels.set(labels);
 }
