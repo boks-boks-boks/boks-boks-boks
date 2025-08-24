506776a2d61d8a13057cc55bc3637d6bb9aafa60
vincentastolfi <vincent.astolfi@etu.uca.fr>
2025-08-24 11:50:07 +0200

    fix: resolve api base url depend on env

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,8 @@
 // place files you want to import through the `$lib` alias in this folder.
-export const baseUrl = import.meta.env.VITE_BASE_URL || 'http://localhost:8080';
+export const baseUrl =
+    import.meta.env.MODE === "development"
+    ? "http://localhost:8080"
+    : "/api";
 
 export * from './api';
 export * from './stores/auth';
