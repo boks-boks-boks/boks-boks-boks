69358e8e6dd73dfac0a75974f51679502ba18815
viastolfi <vincent.astolfi12@gmail.com>
2026-05-30 09:21:18 +0200

    feat: add redirection when scanning box without being connected

--- src/routes/box/[boxId]/items/+page.server.ts
+++ src/routes/box/[boxId]/items/+page.server.ts
@@ -1,6 +1,10 @@
+import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ params }) => {
+export const load: PageServerLoad = async ({ params, locals, url }) => {
+	if (!locals.jwt) {
+		redirect(307, `/login?redirectTo=${encodeURIComponent(url.pathname)}`);
+	}
 	return {
 		boxId: params.boxId
 	};
